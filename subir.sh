57c818d2511aca8795256af37a96f8114bd3d69f
Fernando Real cisneros <fernando.real.cisneros@gmail.com>
2026-05-02 01:29:12 +0200

    Prueba2

--- subir.sh
+++ subir.sh
@@ -1,4 +1,6 @@
 git add . && 
 git commit -a -m "$1" && 
+cd /Users/fernandorealcisneros/trabajo/master
 git push && git subtree push --prefix GENERACION_CODIGO/PARCIALES/PEP4/ProyectoBase github main
+cd /Users/fernandorealcisneros/a
 
